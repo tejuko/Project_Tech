fa57f5eacc12b1ca9cea81f78377e9db6d4a5cb0
tejuko <tessjuliette02@gmail.com>
2026-02-09 14:22:30 +0100

    update

--- RickAndMorty/les3/server.js
+++ RickAndMorty/les3/server.js
@@ -48,23 +48,7 @@ app.get('/profile/:username', (req, res) => {
     return res.status(404).send('Gebruiker niet gevonden');
   }
 
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Profiel van ${user.username}</title>
-      <link rel="stylesheet" href="/css/style.css">
-    </head>
-    <body>
-      <div class="profile-card">
-        <h1>${user.username}</h1>
-        <p><strong>Leeftijd:</strong> ${user.age}</p>
-        <p><strong>Bio:</strong> ${user.bio}</p>
-        <a href="/" class="back-button">Terug naar home</a>
-      </div>
-    </body>
-    </html>
-  `);
+  res.sendFile(__dirname + '/views/profile.html')
 });
 
 // 404
